c67fd711dbbd24500d65e0fa39c1047dbd805e8d
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-03-15 13:02:28 -0600

    Add tests for creating a food
    Tests cover that a food is saved to local storage and displayed in the
    table.

--- test/foods-test/create-food.js
+++ test/foods-test/create-food.js
@@ -1,8 +1,10 @@
 describe('#create-form', function() {
   var $;
+  var localStorage;
 
   before(function(){
     $ = document.getElementById("foods-frame").contentWindow.$;
+    localStorage = document.getElementById("foods-frame").contentWindow.localStorage;
   })
 
   beforeEach(function() {
@@ -27,7 +29,7 @@ describe('#create-form', function() {
       assert.equal(caloriesValidationContent, "Please Enter Calories");
     });
 
-    it('will clear fields after pressing add food', function() {
+    it('will clear validations after adding a food', function() {
       $('#name-field input').val('Banana');
       $('#calories-field input').val('35');
       $('#add-food').click();
@@ -37,6 +39,35 @@ describe('#create-form', function() {
 
       var caloriesValidationContent = $("#calories-field .validation-error").text();
       assert.equal(caloriesValidationContent, "");
+    });
+  });
+
+  context('adding a food', function() {
+
+    beforeEach(function() {
+      console.log(localStorage.clear());
+      console.log(localStorage);
+      food = {name: 'Banana', calories: '35'}
+      $('#name-field input').val(food.name);
+      $('#calories-field input').val(food.calories);
+      $('#add-food').click();
+    });
+
+    it('will save the food to local storage', function() {
+      var savedFoods = JSON.parse(localStorage.getItem('foodList'));
+      var savedFood = savedFoods[0];
+
+      assert.isArray(savedFoods);
+      assert.isObject(savedFood);
+      assert.deepEqual(savedFood, food);
+    });
+
+    it('will add the food to the table', function() {
+      var displayedName = $('.food-name').first().text();
+      var displayedCalories = $('.food-calories').first().text();
+
+      assert.equal(displayedName, food.name);
+      assert.equal(displayedCalories, food.calories);
     });
   });
 });
